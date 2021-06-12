460c35d4363da52760c4f09857df9262d31621c8
Carter David Henning <carterdavidhenning@gmail.com>
2021-06-12 15:01:52 -0400

    WIP: WIP commit as backup before break

--- Assets/Scripts/SpaceStation.cs
+++ Assets/Scripts/SpaceStation.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 
 public class SpaceStation : MonoBehaviour
 {
+    [SerializeField] // Serialized for debugging
+    private StationModule[,] modules;
+    public Vector2Int maxStationDimensions = new Vector2Int(13, 13);
+    public float moduleToGridScaleFactor = 1; // How large a modules array position is in square units
+
+    private void Awake()
+    {
+        modules = new StationModule[maxStationDimensions.x, maxStationDimensions.y];
+    }
+
+    public IEnumerator AddModule(GameObject module, int x, int y)
+    {
+        // Convert grid coords to real coords
+        Vector3 spawnPosition = new Vector3(x * moduleToGridScaleFactor, y * moduleToGridScaleFactor, 0f);
+
+        // Spawn module
+        // Add as child to this
+        // Disable all physics
+        // Shrink to 0
+        // Start coroutine to scale up and wait for it to finish
+        yield return null; // put coroutine here
+        // Add any connection visuals
+        // Enable all physics
+        // 
+    }
+
     public List<StationModule> GetModules(bool onlyModulesWithOpenConnections = false)
     {
         List<StationModule> fullList = GetComponentsInChildren<StationModule>(false).ToList();
