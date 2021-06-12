9e6f63ebf6e614556c85e2458f217173b565c78f
Sternbery <stefansternberg@gmail.com>
2021-06-12 13:07:50 -0400

    Added MeterBar for fueltank see PlayerMovementTestScene

---
+++ Assets/MeterBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterBar : MonoBehaviour
+{
+    Slider slider;
+
+    void Start(){
+        slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxAmount(float amount){
+        slider.maxValue = amount;
+    }
+    public void SetAmount(float amount){
+        slider.value = amount;
+    }
+}
