fc8f54dcdfa829b324189dbf31c7aaa159e61f12
Ahnaf Hassan <absuloutezero@gmail.com>
2021-08-15 22:01:41 -0400

    Controls Settings
    Settings to explain controls and choosing between keyboard and mouse controls.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public CanvasGroup startButton;
     public CanvasGroup loadingBar;
     public CanvasGroup creditsGroup;
+    public CanvasGroup controlsGroup;
 
     public AudioListener ears;
     public DebrisField debrisField;
@@ -26,7 +28,25 @@ public class MainMenu : MonoBehaviour
     [SerializeField]
     float unloadElapsed = 0f;
 
+    private SaveData data;
+    [SerializeField] private TMP_Text highScore;
+    [SerializeField] private Slider controlsSlider;
+    [SerializeField] private TMP_Text mControl1;
+    [SerializeField] private TMP_Text mControl2;
+    [SerializeField] private TMP_Text kControl1;
+    [SerializeField] private TMP_Text kControl2;
+
     //public DebrisField.SpawnMetrics[] meteorStart;
+
+    private void Start()
+    {
+        data = SaveSystem.Load();
+        if(data.highScore > 0)
+            highScore.text = data.highScore.ToString();
+        if (data.keyBoardControls)
+            controlsSlider.value = 1;
+            
+    }
 
     public void StartGame(){
         loadingOp = SceneManager.LoadSceneAsync(startingScene, LoadSceneMode.Additive);
@@ -41,13 +61,40 @@ public class MainMenu : MonoBehaviour
     public void ShowCredits(){
         Hide(startButton);
         Hide(loadingBar);
+        Hide(controlsGroup);
         Show(creditsGroup);
+    }
+    public void ShowControls()
+    {
+        Hide(startButton);
+        Hide(loadingBar);
+        Show(controlsGroup);
+        Hide(creditsGroup);
     }
     public void ShowStartMenu(){
         Show(startButton);
         Hide(loadingBar);
+        Hide(controlsGroup);
         Hide(creditsGroup);
     }
+
+    public void ToggleControls(float controlState)
+    {
+        if (controlState == 0)
+        {
+            kControl1.color = kControl2.color = Color.grey;
+            mControl1.color = mControl2.color = Color.white;
+            data.keyBoardControls = false;
+        }
+        else if (controlState == 1)
+        {
+            kControl1.color = kControl2.color = Color.white;
+            mControl1.color = mControl2.color = Color.grey;
+            data.keyBoardControls = true;
+        }
+        SaveSystem.Save(data);
+    }
+
     void Update(){
         if(loadingOp != null){
             if(pauseElapsed<pauseDuration)
@@ -75,4 +122,5 @@ public class MainMenu : MonoBehaviour
         ui.alpha=1f;
         ui.blocksRaycasts=true;
     }
+
 }
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -5,6 +5,7 @@ public class SaveData
     /// The high score for the player
     /// </summary>
     public int highScore;
+    public bool keyBoardControls;
 
     /// <summary>
     /// Creates a sava data object with a high score of 0
@@ -12,6 +13,7 @@ public class SaveData
     public SaveData()
     {
         highScore = 0;
+        keyBoardControls = false;
     }
 
 }
